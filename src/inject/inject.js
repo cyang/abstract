faa1067986f78d55ad4de13d3c8599ee9607a1bf
Christopher Yang <cyang001@citymail.cuny.edu>
2016-09-25 05:29:14 -0400

    Get response async

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,29 +1,42 @@
 chrome.extension.sendMessage({}, function(response) {
-	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-		    document.addEventListener("mouseover", function(e){
-     		var link = e.target
-     		var url = link.getAttribute("href")
-     		if (url !== null && url.includes("http")){
-     			console.log("it's link")
-     			var timer = setTimeout(function(){
-     				var opt = {
-				    	TemplateType: "basic",
-				    	title: "Just a test!",
-				    	message: url,
-				    	iconUrl: "icon.png"
-					};
-					chrome.runtime.sendMessage({type: "shownotification", opt: opt}, function(){});
-     			}, 1000);
-     			link.addEventListener('mouseout', function(e){
-     				clearTimeout(timer);
-     			});
-     		}
-		});		
-	}
-	}, 10);
+    var readyStateCheckInterval = setInterval(function() {
+        if (document.readyState === "complete") {
+            clearInterval(readyStateCheckInterval);
+            document.addEventListener("mouseover", function(e) {
+                var link = e.target
+                var url = link.getAttribute("href")
+                if (url !== null && url.includes("http")) {
+                    var oReq = new XMLHttpRequest();
+                    var ngrok = "https://837e89f8.ngrok.io/?url="
+
+                    oReq.addEventListener("load", reqListener);
+                    oReq.open("GET", ngrok + url);
+                    oReq.send();
+
+                    
+                }
+            });
+        }
+    }, 10);
 
 });
 
+function reqListener() {
+	if (this.response !== "none") {
+		var msg = this.response;
 
+		var timer = setTimeout(function() {
+			var opt = {
+				message: msg,
+				iconUrl: "icon.png"
+			};
+			chrome.runtime.sendMessage({
+				type: "shownotification",
+				opt: opt
+			}, function() {});
+		}, 1000);
+		link.addEventListener('mouseout', function(e) {
+			clearTimeout(timer);
+		});
+	}
+}
