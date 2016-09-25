7432e7586b7de8c35950489715b8490d564cb644
zhik <zhikhe@gmail.com>
2016-09-25 02:18:40 -0400

    timer

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,21 +2,28 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-     	document.addEventListener("mouseover", function(e){
- 			var link = e.target.getAttribute("href");
-			if (link !== null && link.includes("http")) {
-				console.log(link);
-				var opt = {
-				    TemplateType: "basic",
-				    title: "Just a test!",
-				    message: link,
-				    iconUrl: "icon.png"
-				};
-				chrome.runtime.sendMessage({type: "shownotification", opt: opt}, function(){});
-			}
-     	});  		
+		    document.addEventListener("mouseover", function(e){
+     		var link = e.target
+     		var url = link.getAttribute("href")
+     		if (url !== null && url.includes("http")){
+     			console.log("it's link")
+     			var timer = setTimeout(function(){
+     				var opt = {
+				    	TemplateType: "basic",
+				    	title: "Just a test!",
+				    	message: url,
+				    	iconUrl: "icon.png"
+					};
+					chrome.runtime.sendMessage({type: "shownotification", opt: opt}, function(){});
+     			}, 1000);
+     			link.addEventListener('mouseout', function(e){
+     				clearTimeout(timer);
+     			});
+     		}
+		});		
 	}
 	}, 10);
 
 });
+
 
