0b55a551013c146c128cf60547bbfcfb590d4ef1
Christopher Yang <cyang001@citymail.cuny.edu>
2016-09-25 11:36:34 -0400

    Click for longer message

--- src/bg/background.js
+++ src/bg/background.js
@@ -1,5 +1,11 @@
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     if(request.type === "shownotification"){
-       new Notification(request.opt.message);
+       	var notification = new Notification(request.opt.message);
+       	notification.onclick = function(event) {
+       		// click for longer message
+
+		  	event.preventDefault(); // prevent the browser from focusing the Notification's tab
+		  	alert(request.opt.message);
+		}
     }
 });
