7c795a369114bf8816ff51d7a982c8d2a1535247
zhik <zhikhe@gmail.com>
2016-09-25 06:26:53 -0400

    timer

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,18 +2,46 @@ chrome.extension.sendMessage({}, function(response) {
     var readyStateCheckInterval = setInterval(function() {
         if (document.readyState === "complete") {
             clearInterval(readyStateCheckInterval);
+
+            //mouseover 
             document.addEventListener("mouseover", function(e) {
                 var link = e.target
                 var url = link.getAttribute("href")
+
+                //check if there is a links
                 if (url !== null && url.includes("http")) {
+
+                	//send a request to ""api""
                     var oReq = new XMLHttpRequest();
                     var ngrok = "https://837e89f8.ngrok.io/?url="
+                    var timer = setTimeout(function() {	
+                    	oReq.addEventListener("load", function() {
+                   		//check if api reponse is not none
+                    		if (this.response !== "none") {
+                    		//render out 
+								var msg = this.response;
 
-                    oReq.addEventListener("load", reqListener);
+								alert(msg);
+								// var opt = {
+								// 	message: msg,
+								// 	iconUrl: "icon.png"
+								// };
+								// chrome.runtime.sendMessage({
+								// type: "shownotification",
+								// opt: opt
+								// });
+
+							}
+                    	});
                     oReq.open("GET", ngrok + url);
                     oReq.send();
+                    }, 500);
 
-                    
+                    //kill message request if mouse is moved off link
+                   	link.addEventListener('mouseout', function(e) {
+						clearTimeout(timer);
+					});
+
                 }
             });
         }
@@ -21,22 +49,6 @@ chrome.extension.sendMessage({}, function(response) {
 
 });
 
-function reqListener() {
-	if (this.response !== "none") {
-		var msg = this.response;
 
-		var timer = setTimeout(function() {
-			var opt = {
-				message: msg,
-				iconUrl: "icon.png"
-			};
-			chrome.runtime.sendMessage({
-				type: "shownotification",
-				opt: opt
-			}, function() {});
-		}, 1000);
-		link.addEventListener('mouseout', function(e) {
-			clearTimeout(timer);
-		});
-	}
-}
+
+
