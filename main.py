a000d56a570243b76c2f63e35dc9226c6b14f0d3
zhik <zhikhe@gmail.com>
2016-09-25 05:32:15 -0400

    added route

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
-from flask import jsonify
-from flask import Flask
-from flask import request
-# from textparser import parser
+from flask import Flask, request, jsonify
+from pyteaser import Summarize
+#https://github.com/xiaoxu193/PyTeaser
+from goose import Goose
+#https://github.com/grangier/python-goose
 
 app = Flask(__name__)
 @app.route('/', methods=['GET'])
 def get_summary():
-  url = request.args.get('url')
-  #text = parser(url)
-  # response = unirest.get("https://community-smmry.p.mashape.com/?SM_API_KEY=279153A47B&SM_LENGTH=2&SM_URL={}".format(url),
-  # headers={
-  #   "X-Mashape-Key": "UHmxbuQKVWmsh69rxNfyDIPjObcVp1DhFdQjsnR0gUpePgdDUw",
-  #   "Accept": "text/plain"
-  # # })
-  response = str(html)
-  d = json.loads(html)
-  return d["sm_api_message"]
+  try:
+    url = request.args.get('url')
+    g = Goose({'browser_user_agent': 'Mozilla/5.0 (compatible; MSIE 10.6; Windows NT 6.1; Trident/5.0; InfoPath.2; SLCC1; .NET CLR 3.0.4506.2152; .NET CLR 3.5.30729; .NET CLR 2.0.50727) 3gpp-gba UNTRUSTED/1.0', 'parser_class':'soup'})
+    article = g.extract(url=url)
+    title = article.title
+    text = article.cleaned_text
+    summaries = Summarize(title, text)
+    stringtext = ""
+    for i in summaries:
+      stringtext= stringtext + " "+ i 
+    return jsonify(stringtext)
+  except:
+    return jsonify('none')
 
-  
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+  app.run(debug=True, host='0.0.0.0')
