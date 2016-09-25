baa0b878abd1590ae1718dd8726498a6160dbfe9
Christopher Yang <cyang001@citymail.cuny.edu>
2016-09-25 02:01:27 -0400

    Notifications work

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -2,19 +2,18 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
-		// ----------------------------------------------------------
-		// This part of the script triggers when page is done loading
-		console.log("Hello. This message was sent from scripts/inject.js");
-		// ----------------------------------------------------------
-
      	document.addEventListener("mouseover", function(e){
-     		setTimeout(function() {
-     			var link = e.target.getAttribute("href");
- 				if (link !== null && link.includes("http")) {
- 					console.log(link);
- 				}
-     		}, 2000);
+ 			var link = e.target.getAttribute("href");
+			if (link !== null && link.includes("http")) {
+				console.log(link);
+				var opt = {
+				    TemplateType: "basic",
+				    title: "Just a test!",
+				    message: link,
+				    iconUrl: "icon.png"
+				};
+				chrome.runtime.sendMessage({type: "shownotification", opt: opt}, function(){});
+			}
      	});  		
 	}
 	}, 10);
